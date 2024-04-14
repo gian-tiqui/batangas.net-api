d614267cfd23b29786a49a3e12f291a5fb5efd4e
gian-tiqui <michaelgiantiqui3@gmail.com>
2024-04-14 14:24:37 +0800

    batangas.net news api

---
+++ app/Models/News.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    protected $fillable = [
+        'source_id', 
+        'source_name', 
+        'author',
+        'title',
+        'description',
+        'url',
+        'url_to_image',
+        'published_at',
+        'content',
+    ];
+}
--- routes/api.php
+++ routes/api.php
@@ -8,5 +8,5 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('/news', function (Request $request) {
-    return null;
+    return null; 
 });
